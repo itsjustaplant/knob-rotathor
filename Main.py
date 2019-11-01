d325859ea2f40ae4e4adba3433aa740cfe3749f8
itsjustaplant <alperensahin442@gmail.com>
2019-11-01 13:22:17 +0300

    color channel bug fixed

--- Main.py
+++ Main.py
@@ -1,20 +1,15 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/circle3.png',cv2.IMREAD_UNCHANGED)
-img = cv2.cvtColor(img,cv2.cv2.COLOR_RGBA2BGRA)
+img = cv2.imread('images/knob.png',cv2.IMREAD_UNCHANGED)
+img = cv2.cvtColor(img,cv2.cv2.COLOR_RGB2RGBA)
 (h, w) = img.shape[:2]
-angle90 = 90
-angle180 = 180
-angle270 = 270
-
-
 scale = 1.0
 center = (w / 2, h / 2)
 n=10
 
 for i in range(9):
-    angle = 360/n
+    angle = 45
     M= cv2.getRotationMatrix2D(center,angle*(i+1),scale)
     name = "knob_" + str(i+1) + ".png"
     rotated_image=cv2.warpAffine(img,M,(w,h))
